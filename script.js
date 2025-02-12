fb8dfca323ea95445d07fcaa16faca48ab03b972
cozene <cozenekemboi@gmail.com>
2025-02-12 21:29:40 +0300

    firstpractise

---
+++ script.js
@@ -0,0 +1,17 @@
+document.body.innerHTML = '<h1>Working with Javascript...</h1>';
+
+let favdrink='mango'
+console.log(favdrink)
+
+let favnun=15;
+console.log(15)
+
+let myfans = function() {
+         console.log('myfans');
+}
+
+let character={
+    name:"cozene",
+    Age:10,
+}
+console.log(character)
